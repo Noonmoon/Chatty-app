3e6582cfbf677106c7bf7e9c542866abc88061b6
Tristan <tristandeering@hotmail.com>
2018-11-29 17:46:17 +0000

    messages displaying with current user

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,66 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBarComponent.jsx';
 import MessageList from './MessageListComponent.jsx';
-
+import RNG from '../generateRNG.js';
 
 class App extends Component {
   constructor(props) {
     super(props)
 
+    this.updateUser = this.updateUser.bind(this)
+    this.addMessage = this.addMessage.bind(this)
+
     this.state = {
       currentUser: {name: 'Bob'},
       messages: [
         {
-          id: 1,
+          type: 'incomingMessage',
+          id: RNG(),
           username: 'Bob',
           content: 'Has anyone seen my marbles?',
         },
         {
-          id: 2,
+          type: 'incomingMessage',
+          id: RNG(),
           username: 'Anonymous',
           content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
         }
       ]
     }
   }
+
+  updateUser(e) {
+    let userName = e.target.value;
+    this.setState({ currentUser: {name: userName}})
+  }
+
+  addMessage(name, message) {
+    if (name === '') {
+      name = 'Anonymous'
+    }
+    const newMessage = {
+      type: 'incomingMessage',
+      id: RNG(),
+      username: name,
+      content: message
+    }
+    const oldState = this.state.messages;
+    const newState = [...oldState, newMessage];
+    this.setState({ messages: newState });
+  }
+
+  componentDidMount() {
+
+
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    const newMessage = {id: RNG(), username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+    this.setState({messages: messages})
+  }, 3000);
+}
+
 
   render() {
     return (
@@ -31,9 +69,9 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
 
-        <MessageList messages={this.state.messages}/>
+        <MessageList messages={this.state.messages} />
 
-        <ChatBar user={this.state.currentUser.name} />
+        <ChatBar user={this.state.currentUser.name} updateUser={this.updateUser} addMessage={this.addMessage}/>
       </div>
     );
   }
--- src/ChatBarComponent.jsx
+++ src/ChatBarComponent.jsx
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 
 
 class ChatBar extends Component {
+  onKeyPress = event => {
+    if (event.key == 'Enter'){
+      const user = this.props.user;
+      let inputMessage = event.target;
+      this.props.addMessage(user, inputMessage.value);
+      inputMessage.value = '';
+    }
+  }
   render() {
     return (
       <div>
         <footer className="chatbar">
-          <input className="chatbar-username" placeholder={this.props.user}/>
-          <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+          <input className="chatbar-username" name='user' placeholder='Enter name (optional)' onChange={this.props.updateUser} defaultValue={this.props.user}/>
+          <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.onKeyPress}/>
         </footer>
       </div>
     )
