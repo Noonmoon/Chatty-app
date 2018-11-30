260aac4e69cc859340aff2065dab4d96adcedaf0
Tristan <tristandeering@hotmail.com>
2018-11-30 22:31:23 +0000

    final changes

--- ws_server/server.js
+++ ws_server/server.js
@@ -2,21 +2,15 @@ const express = require('express');
 const WebSocket = require('ws');
 const SocketServer = WebSocket.Server;
 
-// Set the port to 3001
 const PORT = 3003;
 
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
   let userCount = {
@@ -47,8 +41,6 @@ wss.on('connection', (ws) => {
     })
   });
 
-
-  // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     console.log('Client disconnected')
     let userCount = {
